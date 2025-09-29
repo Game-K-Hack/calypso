aca01e97cdde3b4631ff5ef433ef8020f5d6cb3f
Game K <hack.h@laposte.net>
2025-09-29 02:08:11 +0200

    Added VoirAnime and VoirDrama services

--- calypso/service/__init__.py
+++ calypso/service/__init__.py
@@ -2,3 +2,5 @@ from .coomer import Coomer
 from .gotanynudes import Gotanynudes
 from .leakedzone import Leakedzone
 from .openfoodfacts_v3 import OpenFoodFactsV3
+from .voiranime import VoirAnime
+from .voirdrama import VoirDrama
